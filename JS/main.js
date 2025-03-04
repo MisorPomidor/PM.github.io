3d52d6ccaf14c6d8a8046327d30589371ac7e4aa
MisorPomidor <109895173+MisorPomidor@users.noreply.github.com>
2025-03-04 18:57:29 +0200

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
-function toggleMenu() {
-    document.getElementById("sideMenu").classList.toggle("open");
-}
+document.querySelector(".menu-btn").addEventListener("click", function () {
+    document.querySelector(".menu").classList.toggle("open");
+    document.querySelector(".menu-wrapper").classList.toggle("open");
+});
