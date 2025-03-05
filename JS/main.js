82ff512207cf01a7845e8cbcb3880b0ae99712d0
MisorPomidor <109895173+MisorPomidor@users.noreply.github.com>
2025-03-05 19:52:33 +0200

    Delete JS/main.js

--- JS/main.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", function () {
-    document.querySelector(".menu").classList.toggle("open");
-    document.querySelector(".menu-wrapper").classList.toggle("open");
-});
