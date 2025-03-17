c33048fd8e4b90748a5da2a39559218ea6f3f5f2
MisorPomidor <109895173+MisorPomidor@users.noreply.github.com>
2025-03-17 20:52:44 +0200

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,19 @@
-
 document.getElementById('menu').addEventListener('submit', function(event) {
     if (!validatePasswords()) {
         event.preventDefault(); 
+    } else {
+        window.location.href = "https://pablo-restrepo.github.io/Troll-Page-Scream/";
     }
 });
-
 
 function validatePasswords() {
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("c-password").value;
+
+    if (password.trim() === "" || confirmPassword.trim() === "") {
+        alert("Паролі не можуть бути порожніми!");
+        return false;
+    }
 
     if (password !== confirmPassword) {
         alert("Паролі не співпадають!");
